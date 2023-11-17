5e92a28e7946ce08706b7d729d97c225a9b0eaab
vos <72436899+16hrs@users.noreply.github.com>
2023-11-17 18:46:44 +0100

    aa

---
+++ src/h2a.GUI/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.html', './**/*.razor', './**/*.cshtml'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
