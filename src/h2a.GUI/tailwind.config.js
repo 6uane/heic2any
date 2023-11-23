4a90e173a5f156dd92bce4ed22ed6fd2b82aa549
vos <72436899+16hrs@users.noreply.github.com>
2023-11-23 16:11:33 +0100

    messing around with styling

--- src/h2a.GUI/tailwind.config.js
+++ src/h2a.GUI/tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
     content: ['./**/*.html', './**/*.razor', './**/*.cshtml'],
     theme: {
         extend: {},
+        colors: {
+            "honeydew": "#EFF8E2",
+            "eerie": "#222222",
+            "ultraviolet": "#4B4E6D",
+            "tiffanyblue": "#84DCC6",
+            "cadetgray": "#95A3B3",
+            "white": "#FFFFFF",
+            "whitish": "#cbccc4",
+            "stoney": "#32322C",
+            "greyish": "#b1b59b",
+            "blueish": "#00917d",
+            "darker": "#292a23"
+        }
     },
     plugins: [],
 }
